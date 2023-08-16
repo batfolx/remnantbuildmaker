71b0bc2f0bbb48dc7c0fdc3ccfdcfac57b6d350a
Cosmin Velea <victorvelea007@yahoo.com>
2023-08-16 18:04:26 -0400

    Import builds from URL

--- src/components/ViewUrlBuild.js
+++ src/components/ViewUrlBuild.js
@@ -1,7 +1,21 @@
 import {useParams, useNavigate} from "react-router-dom";
 import ReactGA from "react-ga4";
 import {useEffect, useState} from "react";
-import {AppBar, Box, CircularProgress, IconButton, Toolbar, Tooltip, Typography} from "@mui/material";
+import {
+    AppBar,
+    Box,
+    Button,
+    CircularProgress,
+    Dialog,
+    DialogActions,
+    DialogTitle,
+    DialogContent,
+    IconButton,
+    Toolbar,
+    Tooltip,
+    Typography,
+    MenuItem, Select
+} from "@mui/material";
 import {toast, ToastContainer} from "react-toastify";
 import 'react-toastify/dist/ReactToastify.min.css';
 import RemnantBuildWebApi from "../buildWebApi";
@@ -13,10 +27,18 @@ import {
 } from "../utilFunctions";
 import ExitToAppIcon from "@mui/icons-material/ExitToApp";
 import HomeIcon from '@mui/icons-material/Home';
+import {UploadFile} from "@mui/icons-material";
+import {useDispatch, useSelector} from "react-redux";
+import {actions} from "../reducers/loadoutReducer";
+import {sendImportSingleBuildEvent} from "../constants";
 export default function ViewUrlBuild() {
 
+    const dispatch = useDispatch();
+    const loadouts = useSelector((state) => state.loadouts);
     const [loadoutData, setLoadoutData] = useState(null);
     const [fetchingLoadout, setFetchingLoadout] = useState(true);
+    const [selectedBuild, setSelectedBuild] = useState(loadouts.loadouts[0]);
+    const [openImportBuild, setOpenImportBuild] = useState(false);
     const {buildId} = useParams();
     const navigate = useNavigate();
 
@@ -48,6 +70,13 @@ export default function ViewUrlBuild() {
         }
     }
 
+    const overwriteBuild = (buildData) => {
+        const buildDataCopy = {...buildData};
+        delete buildDataCopy.buildType;
+        const index = loadouts.loadouts.indexOf(selectedBuild);
+        dispatch(actions.overwriteBuild({index: index, buildData: buildDataCopy}));
+        sendImportSingleBuildEvent();
+    }
 
 
     return (
@@ -62,6 +91,16 @@ export default function ViewUrlBuild() {
                     >
                         Remnant Builder
                     </Typography>
+                    <IconButton onClick={() => {
+                        if (!loadoutData) {
+                            return;
+                        }
+                        setOpenImportBuild(true)
+                    }}>
+                        <Tooltip title={"Import This Build"}>
+                            <UploadFile/>
+                        </Tooltip>
+                    </IconButton>
 
                     <IconButton onClick={() => {navigate("/")}}>
                         <Tooltip title={"Back to Home"}>
@@ -79,7 +118,6 @@ export default function ViewUrlBuild() {
             <ToastContainer/>
             {fetchingLoadout && <Box marginTop={"5%"} display={'flex'} justifyContent={'center'}><CircularProgress/></Box>}
             {loadoutData !== null && (
-
                 <Box marginTop={"25px"}>
                     {loadoutData.rings && <Box display={'flex'} flexDirection={'column'}>
                         {getHeaderComponent("Rings")}
@@ -126,6 +164,40 @@ export default function ViewUrlBuild() {
 
                 </Box>
             )}
+
+            <Dialog open={openImportBuild} fullWidth maxWidth={'xl'} onClose={(event, reason) => {setOpenImportBuild(false)}}>
+                <DialogTitle>
+                    Import this build
+                </DialogTitle>
+                <DialogContent>
+                    <Select fullWidth value={selectedBuild} onChange={e => setSelectedBuild(e.target.value)}>
+                        {loadouts.loadouts.map((loadout, index) => {
+                            return (<MenuItem key={index + loadout.loadoutName} value={loadout}>
+                                Build {index + 1}, {loadout.loadoutName === "" ? "No Loadout Name" : loadout.loadoutName}
+                                </MenuItem>)
+                        })}
+                    </Select>
+                    <DialogActions>
+
+                        <Button variant={'contained'} onClick={() => {
+                            overwriteBuild(loadoutData)
+                            toast.success("Successfully imported build!");
+                            setOpenImportBuild(false);
+                        }}>
+                            Import
+                        </Button>
+
+                        <Button variant={'contained'} color={'primary'} onClick={() => setOpenImportBuild(false)}>
+                            Close
+                        </Button>
+
+
+                    </DialogActions>
+
+
+                </DialogContent>
+
+            </Dialog>
 
 
         </Box>
